17e605be7a1c16256e82b0296523d23879ed2ff3
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-14 16:08:33 +0530

    [Create simple Linkelist]

---
+++ UC-1SimpleLinkedList/LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_1SimpleLinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int id)
+        {
+            Node node = new Node(id);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                temp.next = node;
+
+
+
+            }
+            Console.WriteLine("{0} is inserted int linked list", node.id);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.id + "");
+                temp = temp.next;
+            }
+        }
+        
+
+
+    }
+}
