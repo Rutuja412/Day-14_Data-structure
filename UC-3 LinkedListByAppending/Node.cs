361ff7ab62966b92173535da7f0ca40710fe8180
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-14 17:01:53 +0530

    [Create Linked List by appending]

---
+++ UC-3 LinkedListByAppending/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_4InsertLinkedList
+{
+    public class Node
+    {
+        public int id;
+        public Node next;
+        public Node(int id)
+        {
+            this.id = id;
+        }
+    }
+}
---
+++ UC-3 LinkedListByAppending/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UC_4InsertLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+        }
+    }
+}
