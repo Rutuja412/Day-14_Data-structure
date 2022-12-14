d0f8c4d18f9a5b322a06dc0a769b9f5ca195c4ac
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-14 17:49:47 +0530

    [Insert 30 between 56 & 70]

---
+++ UC-4AbilityInsert/LinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_4AbilityInsert
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int id)
+        {
+            Node node = new Node(id);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                temp.next = node;
+
+
+
+            }
+            Console.WriteLine("{0} is inserted int linked list", node.id);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.id + "");
+                temp = temp.next;
+            }
+        }
+        public Node InsertAtParticularPosition(int position,int id)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid Position");
+            if (position == 1)
+            {
+                var newNode = new Node(id);
+                newNode.next = this.head;
+                head = newNode;
+                
+            }
+            else 
+            {
+                while (position-- !=0) 
+                {
+                if (position==1) 
+                    {
+                    Node node=new Node(id);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+
+                    }
+                   head=head.next;
+                }
+                if (position != 1)
+                {
+                    Console.WriteLine("Position out of range");
+
+                }
+                Console.WriteLine("inserted value is:" + head);
+                return head;
+            }
+        }
+ }  }
+
+   
---
+++ UC-4AbilityInsert/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC_4AbilityInsert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            // linkedList.Add(30);
+            linkedList.InsertAtParticularPosition(2, 20);
+            linkedList.Add(70);
+            linkedList.Display();
+        }
+    }
+}
